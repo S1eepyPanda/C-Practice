b86a2e8ef63eb4791bdf5ade747b8cff2249a10f
S1eepyPanda <127610416+S1eepyPanda@users.noreply.github.com>
2024-05-27 20:15:57 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+ * sbyte - 1B - -128 to 127
+ * byte  - 1B - 0 to 255
+ * short - 2B - -32 768 to 32 767
+ * ushort - unsigned to 65 535
+ * int - 4B - -2 147 483 648 to 2 147 483 647
+ * uint - unsigned - 0 to 4 294 967 295
+ * long - 8B -  дохуя
+ * ulong - 0 to слишком дохуя
+ * float - 4B 
+ * double - 8B
+ * enum Name:int
+ * {
+ * ex1=0,
+ * ex2=43,
+ * ex3=76,
+ * }
+ * int x = 3, y = 4;
+Console.WriteLine("x={0} y={1}", x, y); на место параметра {0} выводится значение x и т.д.
+double x math.E;
+Console.WriteLine("E={0,20}", x);   (2,718...)
+Console.WriteLine("E={0,10}", x);(2,718...)
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+double x math.E;
+Console.WriteLine("E={0:##.###}", x);E=2,718
+Console.WriteLine("E={0:.####}", x); E=2,7183
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+Console.WriteLine("C format:{0,14:C} \t{0:C2}, 12345.678" ||C format: 12 345,68p.           12345,68p.
+\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+sting s = Console.ReadLine(); 
+Console.WriteLine(s);         
+\\
+string s = Console.ReadLine();
+int x = int.Parse(s);
+Console.WriteLine(x);
+OR
+int x = int.Parse(Console.ReadLine()); преобразовывает строку в числовой формат int в данном примере
+Console.WriteLine(x);
+ */
+using System.Globalization;
+using System.Text;
+
+Console.OutputEncoding = Encoding.Unicode;
+Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("ru-RU");
+static int Main()
+{
+    string method = Console.ReadLine();
+    int x, y, z;
+    switch (method)
+    {
+        case "+":
+             x = int.Parse(Console.ReadLine());
+             y = int.Parse(Console.ReadLine());
+             z = x + y;
+            Console.WriteLine(z);
+            break;
+        case "show":
+             x = int.Parse(Console.ReadLine());
+             y = int.Parse(Console.ReadLine());
+            break;
+    }
+    return 0;
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+namespace Myapp
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            int x;
+            int y;
+            int z;
+            int s;
+            float a; float b; float c; float d;
+            string Name;
+            int Age;
+            Console.WriteLine("Введите номер задания");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("-------------------------------------------------------");
+                string method = Console.ReadLine();
+                Console.WriteLine("-------------------------------------------------------");
+                switch (method)
+                {
+                    case "1":
+                        {
+                            x = int.Parse(Console.ReadLine());
+                            y = int.Parse(Console.ReadLine());
+                            z = x + y;
+                            Console.WriteLine("a={0}\nb={1}", x, y);
+                            Console.WriteLine("{0}+{1}={2}", x, y, z);
+                            break;
+                        }
+                    case "2":
+                        {
+                            x = int.Parse(Console.ReadLine());
+                            y = int.Parse(Console.ReadLine());
+                            Console.WriteLine("a={0}\nb={1}", x, y);
+                            Console.WriteLine("{0}+{1}={1}+{0}");
+                            break;
+                        }
+                    case "3":
+                        {
+                            x = int.Parse(Console.ReadLine());
+                            y = int.Parse(Console.ReadLine());
+                            z = int.Parse(Console.ReadLine());
+                            s = x + y + z;
+                            Console.WriteLine("a={0}\nb={1}\nc={2}\n{0}+{1}+{2}={3}", x, y, z, s);
+                            break;
+                        }
+                    case "4":
+                        {
+                            a = float.Parse(Console.ReadLine());
+                            b = float.Parse(Console.ReadLine());
+                            c = a * b;
+                            Console.WriteLine("a={0}\nb={1}\n{0:##.#}*{1:##.#}={2:##.#}", a, b, c);
+                            break;
+                        }
+                    case "5":
+                        {
+                            a = float.Parse(Console.ReadLine());
+                            b = float.Parse(Console.ReadLine());
+                            c = a / b;
+                            Console.WriteLine("a={0:###.###}\nb={1:###.###}\n{0:###.###}/{1:###.###}={2:###.###}", a, b, c);
+                            break;
+                        }
+                    case "6":
+                        {
+                            a = float.Parse(Console.ReadLine());
+                            b = float.Parse(Console.ReadLine());
+                            c = float.Parse(Console.ReadLine());
+                            Console.WriteLine("a={0}\nb={1}\nc={2}\n<{0:##.##}+{1:##.##}>+{2:##.##}-{0:##.##}+<{1:##.##}+{2:##.##}>", a, b, c);
+                            break;
+                        }
+                    case "7":
+                        {
+                            Console.OutputEncoding = Encoding.Unicode;
+                            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("ru-RU");
+                            x = int.Parse(Console.ReadLine());
+                            y = int.Parse(Console.ReadLine());
+                            z = x * y;
+                            Console.WriteLine("Номинал купюры = {0:C}\nКоличество купюр = {1}\nСумма денег = {2:C}", x, y, z);
+                            break;
+                        }
+                    case "8":
+                        {
+                            Console.OutputEncoding = Encoding.Unicode;
+                            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("ru-RU");
+                            Console.Write("Введите сумму вклада = ");
+                            a = float.Parse(Console.ReadLine());
+                            Console.Write("Введите процент по вкладу = ");
+                            b = float.Parse(Console.ReadLine());
+                            b /= 100;
+                            c = a * b;
+                            Console.WriteLine("Через год начислят {0:C}", c);
+                            break;
+                        }
+                    case "9":
+                        {
+                            Console.OutputEncoding = Encoding.Unicode;
+                            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("ru-RU");
+                            Console.Write("Введите сумму вклада = ");
+                            a = float.Parse(Console.ReadLine());
+                            Console.Write("Введите процент по вкладу = ");
+                            b = float.Parse(Console.ReadLine());
+                            b /= 100;
+                            c = (a * b) + a;
+                            Console.WriteLine("Через год начислят {0:C}", c);
+                            break;
+                        }
+                    case "10":
+                        {
+                            Console.Write("Как тебя зовут? ");
+                            Name = Console.ReadLine();
+                            Console.Write("Сколько тебе лет? ");
+                            Age = int.Parse(Console.ReadLine());
+                            Age = 2009 - Age;
+                            Console.WriteLine("{0}, ты родился в {1} году.", Name, Age);
+                            break;
+                        }
+                }
+
+            }
+        }
+    }
+}
